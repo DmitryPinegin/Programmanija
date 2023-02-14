5bf2454f191dfb06b1af594d10da65c9d77c274a
DmitryPinegin <snobmsi@gmail.com>
2023-02-14 15:41:42 +0300

    update package UsingPrimitiveAndReferenceDataTypesWhenCallingAMethod

--- src/UsingPrimitiveAndReferenceDataTypesWhenCallingAMethod/Car.java
+++ src/UsingPrimitiveAndReferenceDataTypesWhenCallingAMethod/Car.java
@@ -3,7 +3,7 @@ package UsingPrimitiveAndReferenceDataTypesWhenCallingAMethod;
 public class Car {
     String color;
     String engine;
-    public static int numberOfDoors;
+    int numberOfDoors;
     Car(String color, String engine, int numberOfDoors) {
         this.color = color;
         this.engine = engine;
@@ -12,10 +12,10 @@ public class Car {
 
 }
 class CarTest {
-    public static void changingTheNumberOfDoors(Car car, int quantity) {
+     static void changingTheNumberOfDoors(Car car, int quantity) {
         car.numberOfDoors = quantity;
     }
-    static void colorReplacement(Car car1, Car car2) {
+     static void colorReplacement(Car car1, Car car2) {
         Car car3 = new Car("Black", "V6", 3);
         car3.color = car1.color;
         car1.color = car2.color;
