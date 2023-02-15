1079741a5eda8612810917aa0e42eda5fcf8696e
DmitryPinegin <snobmsi@gmail.com>
2023-02-15 19:35:39 +0300

    update package The_if_and_if_else_constructs_ternary_operator

--- src/The_if_and_if_else_constructs_ternary_operator/Student.java
+++ src/The_if_and_if_else_constructs_ternary_operator/Student.java
@@ -11,3 +11,35 @@ public class Student {
         this.srednjayaOcenka = srednjayaOcenka;
     }
 }
+class StudentTest {
+    static void sravnenie1(Student st1, Student st2) {
+   //     st1 = st2;
+        if (st1.equals(st2)) {
+            System.out.println("Студенты равны");
+        } else {
+            System.out.println("Студенты не равны");
+        }
+    }
+
+    static void sravnenie2(Student st1, Student st2) {
+        if (st1.name == st2.name) {
+            if (st1.course == st2.course) {
+                if (st1.srednjayaOcenka == st2.srednjayaOcenka) {
+                } else {
+                    System.out.println("Средние оценки студентов не равны");
+                }
+            } else {
+                System.out.println("Студенты учатся на разных курсах");
+            }
+        } else {
+            System.out.println("У студентов разные имена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Student st1 = new Student(1, "Ivan", 4.5);
+        Student st2 = new Student(1, "Ivan", 4.5);
+        sravnenie1(st1, st2);
+        sravnenie2(st1, st2);
+    }
+}
