0548209679b543d70efa124c883737b2650b8f3a
DmitryPinegin <snobmsi@gmail.com>
2023-02-15 19:16:28 +0300

    create package The_if_and_if_else_constructs_ternary_operator

---
+++ src/The_if_and_if_else_constructs_ternary_operator/Student.java
@@ -0,0 +1,13 @@
+package The_if_and_if_else_constructs_ternary_operator;
+
+public class Student {
+    int course;
+    String name;
+    double srednjayaOcenka;
+
+    Student(int course, String name, double srednjayaOcenka) {
+        this.course = course;
+        this.name = name;
+        this.srednjayaOcenka = srednjayaOcenka;
+    }
+}
