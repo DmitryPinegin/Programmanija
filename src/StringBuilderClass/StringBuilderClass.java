ad795e2af3bc64f7c995ef0fa8729f87e3d72724
DmitryPinegin <snobmsi@gmail.com>
2023-02-20 21:55:38 +0300

    update package StringBuilderClass

--- src/StringBuilderClass/StringBuilderClass.java
+++ src/StringBuilderClass/StringBuilderClass.java
@@ -2,12 +2,12 @@ package StringBuilderClass;
 
 public class StringBuilderClass {
     static boolean ravenstvo(StringBuilder s1, StringBuilder s2) {
-        return (s1.equals(s2));
+        return (s1 == (s2));
     }
 
     public static void main(String[] args) {
         StringBuilder s1 = new StringBuilder();
-        StringBuilder s2 = new StringBuilder();
+//        StringBuilder s2 = new StringBuilder();
         StringBuilder s3 = s1;
         System.out.println(ravenstvo(s1, s3));
     }
