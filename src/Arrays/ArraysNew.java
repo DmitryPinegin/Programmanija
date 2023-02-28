7cc19fcbc7ec7a6e9bb3f6eada93a0c96a420d39
DmitryPinegin <snobmsi@gmail.com>
2023-02-28 15:12:03 +0300

    create package ArraysNew

--- src/Arrays/ArraysNew.java
+++ src/Arrays/ArraysNew.java
@@ -2,19 +2,31 @@ package Arrays;
 
 public class ArraysNew {
     static int[] sortirovka(int[] array) {
-        for(int i = 1; i < array.length; i++) {
-            if(array[0] > array[i]) {
-                int num = array[i - 1];
-                array[i - 1] = array[i];
-                array[i] = num;
+        int a;
+
+        for (int i = 0; i < array.length; i++) {
+            int index = i;
+            int min = array[i];
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    index = j;
+                }
+            }
+            if (i != index) {
+                a = array[i];
+                array[i] = min;
+                array[index] = a;
             }
         }
+
         return array;
     }
 
     public static void main(String[] args) {
-        int[] array = new int[]{4, 2, 8, -1, 0, 1, 5, -6, 7, 11};
-        sortirovka(array);
-        for (int j : array) System.out.print(j + " ");
+        int[] arrays = new int[]{4, 2, 8, -1, 0, 1, 5, -6, 7, 11};
+        sortirovka(arrays);
+        for (int j : arrays) System.out.print(j + " ");
     }
 }
