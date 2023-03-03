7f5ee72535adc653970f15a3952e5cace06afa32
DmitryPinegin <snobmsi@gmail.com>
2023-03-03 18:29:48 +0300

    create class ArrayList

---
+++ src/ArrayList/Class1.java
@@ -0,0 +1,12 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Class1 {
+    void abc(String...text) {
+        ArrayList<String> array1 = new ArrayList<>();
+        array1.add(Arrays.toString(text));
+        Arrays.sort(new ArrayList[]{array1});
+    }
+}
