0748ecfd893f4414d483cf9dd1f785b024f3a336
DmitryPinegin <snobmsi@gmail.com>
2023-03-15 19:58:17 +0300

    update class Animal

--- src/Encapsulation/Animal.java
+++ src/Encapsulation/Animal.java
@@ -4,11 +4,19 @@ public class Animal {
     Animal(){
         System.out.println("I am animal");
     }
-    int eyes;
-    void eat() {
+    private static int eyes;
+    public int getEyes() {
+        return eyes;
+    }
+    public static void setEyes(int i) {
+        if(i > 0 && i < 3) {
+            eyes = i;
+        }
+    }
+    public void eat() {
         System.out.println("Animal eats");
     }
-    void drink() {
+    public void drink() {
         System.out.println("Animal drinks");
     }
 }
