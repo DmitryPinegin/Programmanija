eb93a34ff3a5cb90fbb3a714120cbf98ad62c807
DmitryPinegin <snobmsi@gmail.com>
2023-03-15 18:32:50 +0300

    create class Animal

---
+++ src/Encapsulation/Animal.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class Animal {
+    Animal(){
+        System.out.println("I am animal");
+    }
+    int eyes;
+    void eat() {
+        System.out.println("Animal eats");
+    }
+    void drink() {
+        System.out.println("Animal drinks");
+    }
+}
