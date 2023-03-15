865a16e6a6f0d3fec44313f87d7de64e34cc56e3
DmitryPinegin <snobmsi@gmail.com>
2023-03-15 19:58:35 +0300

    update class Pet

--- src/Encapsulation/Pet.java
+++ src/Encapsulation/Pet.java
@@ -1,4 +1,18 @@
 package Encapsulation;
 
-public class Pet {
+public class Pet extends Animal {
+    Pet() {
+        Pet.setEyes(2);
+        System.out.println("I am pet");
+    }
+    String name;
+    int tail = 1;
+    int paw = 4;
+    void run() {
+        System.out.println("Pet runs");
+    }
+    void jump() {
+        System.out.println("Pet jumps");
+    }
+
 }
