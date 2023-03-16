96e8a3e9efa6f34e5e845bfe3b5ac333dfa46725
DmitryPinegin <snobmsi@gmail.com>
2023-03-16 16:34:28 +0300

    create class X

--- src/Overriding_hiding_final/X.java
+++ src/Overriding_hiding_final/X.java
@@ -1,7 +1,7 @@
 package Overriding_hiding_final;
 
 public class X {
-    protected void abc() {
-        System.out.println("X");
-    }
+//    protected void abc() {
+//        System.out.println("X");
+//    }
 }
